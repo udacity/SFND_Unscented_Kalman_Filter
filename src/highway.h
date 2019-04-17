e69c1c29ce648aa063c673ceef73287ca3a756a6
awbrown90 <aaronwbrown@msn.com>
2019-04-17 14:10:47 -0700

    first: adding new ukf project for tracking mulitple virtual cars on a highway

---
+++ src/highway.h
@@ -0,0 +1,117 @@
+#include "render/render.h"
+#include "tools.h"
+
+class Highway
+{
+public:
+
+	std::vector<Car> traffic;
+	Tools tools;
+
+	Highway(pcl::visualization::PCLVisualizer::Ptr& viewer)
+	{
+
+		tools = Tools();
+	
+		Car egoCar(Vect3(0, 0, 0), Vect3(4, 2, 2), Color(0, 1, 0), 0, 0, 2, "egoCar");
+		traffic.push_back(egoCar);
+	
+		Car car1(Vect3(-10, 4, 0), Vect3(4, 2, 2), Color(0, 0, 1), 5, 0, 2, "car1");
+		
+		std::vector<accuation> car1_instructions;
+		accuation a = accuation(0.5*1e6, 0.5, 0.0);
+		car1_instructions.push_back(a);
+		a = accuation(2.2*1e6, 0.0, -0.2);
+		car1_instructions.push_back(a);
+		a = accuation(3.3*1e6, 0.0, 0.2);
+		car1_instructions.push_back(a);
+		a = accuation(4.4*1e6, -2.0, 0.0);
+		car1_instructions.push_back(a);
+	
+		car1.setInstructions(car1_instructions);
+		UKF ukf1;
+		car1.setUKF(ukf1);
+		traffic.push_back(car1);
+	
+		Car car2(Vect3(25, -4, 0), Vect3(4, 2, 2), Color(0, 0, 1), -6, 0, 2, "car2");
+		std::vector<accuation> car2_instructions;
+		a = accuation(4.0*1e6, 3.0, 0.0);
+		car2_instructions.push_back(a);
+		a = accuation(8.0*1e6, 0.0, 0.0);
+		car2_instructions.push_back(a);
+		car2.setInstructions(car2_instructions);
+		UKF ukf2;
+		car2.setUKF(ukf2);
+		traffic.push_back(car2);
+	
+		Car car3(Vect3(-12, 0, 0), Vect3(4, 2, 2), Color(0, 0, 1), 1, 0, 2, "car3");
+		std::vector<accuation> car3_instructions;
+		a = accuation(0.5*1e6, 2.0, 1.0);
+		car3_instructions.push_back(a);
+		a = accuation(1.0*1e6, 2.5, 0.0);
+		car3_instructions.push_back(a);
+		a = accuation(3.2*1e6, 0.0, -1.0);
+		car3_instructions.push_back(a);
+		a = accuation(3.3*1e6, 2.0, 0.0);
+		car3_instructions.push_back(a);
+		a = accuation(4.5*1e6, 0.0, 0.0);
+		car3_instructions.push_back(a);
+		a = accuation(5.5*1e6, -2.0, 0.0);
+		car3_instructions.push_back(a);
+		a = accuation(7.5*1e6, 0.0, 0.0);
+		car3_instructions.push_back(a);
+		car3.setInstructions(car3_instructions);
+		UKF ukf3;
+		car3.setUKF(ukf3);
+		traffic.push_back(car3);
+	
+		// render environment
+		renderHighway(0,viewer);
+		egoCar.render(viewer);
+		car1.render(viewer);
+		car2.render(viewer);
+		car3.render(viewer);
+	}
+	
+	void stepHighway(double egoVelocity, long long timestamp, int frame_per_sec, pcl::visualization::PCLVisualizer::Ptr& viewer)
+	{
+		// ----------------------------------------------------
+		// -----Open 3D viewer and display simple highway -----
+		// ----------------------------------------------------
+	
+		// render environment
+		renderHighway(egoVelocity*timestamp/1e6, viewer);
+		
+		for (int i = 0; i < traffic.size(); i++)
+		{
+			traffic[i].move((double)1/frame_per_sec, timestamp);
+			traffic[i].render(viewer);
+			// Sense surrounding cars with lidar and radar
+			if(i > 0)
+			{
+				VectorXd gt(4);
+				gt << traffic[i].position.x, traffic[i].position.y, traffic[i].velocity*cos(traffic[i].angle), traffic[i].velocity*sin(traffic[i].angle);
+				tools.ground_truth.push_back(gt);
+				tools.lidarSense(traffic[i], viewer, timestamp);
+				tools.radarSense(traffic[i], traffic[0], viewer, timestamp);
+				tools.ukfResults(traffic[i],viewer);
+				VectorXd estimate(4);
+				double v  = traffic[i].ukf.x_(2);
+    			double yaw = traffic[i].ukf.x_(3);
+    			double v1 = cos(yaw)*v;
+    			double v2 = sin(yaw)*v;
+				estimate << traffic[i].ukf.x_[0], traffic[i].ukf.x_[1], v1, v2;
+				tools.estimations.push_back(estimate);
+	
+			}
+		}
+		viewer->addText("Accuracy - RMSE:", 30, 300, 20, 1, 1, 1, "rmse");
+		VectorXd rmse = tools.CalculateRMSE(tools.estimations, tools.ground_truth);
+		viewer->addText(std::to_string(rmse[0]), 30, 275, 20, 1, 1, 1, "rmse_x");
+		viewer->addText(std::to_string(rmse[1]), 30, 250, 20, 1, 1, 1, "rmse_y");
+		viewer->addText(std::to_string(rmse[2]), 30, 225, 20, 1, 1, 1, "rmse_vx");
+		viewer->addText(std::to_string(rmse[3]), 30, 200, 20, 1, 1, 1, "rmse_vy");
+		
+	}
+	
+};
---
+++ src/tools.h
@@ -0,0 +1,56 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+#include "render/render.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+struct lmarker
+{
+	double x, y;
+	lmarker(double setX, double setY)
+		: x(setX), y(setY)
+	{}
+
+};
+
+struct rmarker
+{
+	double rho, phi, rho_dot;
+	rmarker(double setRho, double setPhi, double setRhoDot)
+		: rho(setRho), phi(setPhi), rho_dot(setRhoDot)
+	{}
+
+};
+
+class Tools {
+	public:
+	/**
+	* Constructor.
+	*/
+	Tools();
+	
+	/**
+	* Destructor.
+	*/
+	virtual ~Tools();
+	
+	// Members
+	std::vector<VectorXd> estimations;
+	std::vector<VectorXd> ground_truth;
+	
+	double noise(double stddev);
+	lmarker lidarSense(Car& car, pcl::visualization::PCLVisualizer::Ptr& viewer, long long timestamp);
+	rmarker radarSense(Car& car, Car ego, pcl::visualization::PCLVisualizer::Ptr& viewer, long long timestamp);
+	void ukfResults(Car car, pcl::visualization::PCLVisualizer::Ptr& viewer);
+	/**
+	* A helper method to calculate RMSE.
+	*/
+	VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+	
+};
+
+#endif /* TOOLS_H_ */
